d8748b6c71a3965dddcf6fb241bda5945e4fa3d6
AMIRKIAYI <amirkiayi516@gmail.com>
2024-08-09 22:44:54 +0300

    files update

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,63 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    """Return a list of names of spicy foods."""
+    return [food["name"] for food in spicy_foods]
+# Output: ['Green Curry', 'Buffalo Wings', 'Mapo Tofu']😂😂😂😂
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    """returns a list of dictionaries where the heat level of the food is greater than 5."""
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+# Output: [{'name': 'Green Curry', 'cuisine': 'Thai', 'heat_level
+
+    
 
 def print_spicy_foods(spicy_foods):
-    pass
+    """output to the terminal each spicy food in the following format using print(): Buffalo Wings (American) | Heat Level: 🌶🌶🌶."""
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶🌶🌶'}"
+              f"{'🌶' * (food['heat_level'] - 3)}")
+        # Output: Buffalo Wings (American) | Heat Level: 🌶🌶
+            
+
+        
+
+        
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    """returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method."""
+    return next((food for food in spicy_foods if food["cuisine"] == cuisine),None)
+# Output: {'name': 'Buffalo Wings', 'cuisine': 'American', 'heat
+# level': 3}
+
+    
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    """outputs to the terminal ONLY the spicy foods that have a heat level greater than 5, in the following format: Buffalo Wings (American) | Heat Level: 🌶🌶🌶."""
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶🌶🌶'}"
+                  f"{'🌶' * (food['heat_level'] - 3)}")
+                 
+                  # Output: Green Curry (Thai) | Heat Level: 🌶🌶
+
+
+    
+   
 
 def get_average_heat_level(spicy_foods):
-    pass
+    """returns an integer representing the average heat level of all the spicy foods in the array. """
+    return sum(food["heat_level"] for food in spicy_foods) // len(spicy_foods)
+
+    
+ 
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    """adds a new spicy food to the array of spicy foods and returns the original list with the new spicy_food added."""
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+# Output: [{'name': 'Buffalo Wings', 'cuisine': 'American', 'heat
+# level': 3}, {'name': 'Green Curry', 'cuisine': 'Thai',
+
